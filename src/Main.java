12f306337cf171f5d652714d36dcb4aa9ed9984e
KristofMikle <bitangjooo@gmail.com>
2018-05-29 15:01:39 +0200

    board array initialized

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import com.codecool.termlib.Terminal;
 import com.codecool.termlib.Direction;
 import com.codecool.termlib.Attribute;
 import com.codecool.termlib.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import java.util.Scanner;
 import java.io.InputStream;
@@ -10,7 +13,14 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        //System.out.print("ok");
-    }
+        System.out.print("\u250F\u2501\u2533\u2501\u2513\n\u2503\n\u2523\u2501\u254B\u2501\u252B\n\u2517\u2501\u253B\u2501\u251B");
+		String[][] alist = new String[10][10];
+		for (int i = 0; i < 10; i++) {
+			Arrays.fill(alist[i], " ");
+		}
+		System.out.print(Arrays.toString(alist[2]));
+		String board = new String();
+		
+    	}
     
 }
