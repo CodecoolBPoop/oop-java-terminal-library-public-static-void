fe8cedd730d6f3c598ad185eca68e45628841740
KristofMikle <bitangjooo@gmail.com>
2018-05-29 16:12:35 +0200

    on startup a boards appears

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,36 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("\u250F\u2501\u2533\u2501\u2513\n\u2503\n\u2523\u2501\u254B\u2501\u252B\n\u2517\u2501\u253B\u2501\u251B");
+		//System.out.print("\u2503\u254B");
+        //System.out.print("\u250F\u2501\u2501\u2533\u2501\u2513\n\u2503\n\u2523\u2501\u254B\u2501\u252B\n\u2517\u2501\u253B\u2501\u251B");
 		String[][] alist = new String[10][10];
-		for (int i = 0; i < 10; i++) {
-			Arrays.fill(alist[i], " ");
+		String board = new String();		
+		for (int k = 0; k < 10; k++) {
+			Arrays.fill(alist[k], " ");
 		}
-		System.out.print(Arrays.toString(alist[2]));
-		String board = new String();
+		for (int i = 0; i < 11; i++) {
+			if (i == 0){board += "┏";
+						for (int j = 0; j < 9; j++) {board += "━━━┳";}
+						board += "━━━┓\n";
+						}
+			else if (i == 10) {board += "┃";
+							  for (int j = 0; j < 9; j++) {board += "   ┃";}
+							  board += "   ┃\n";
+							  board += "┗";
+							  for (int j = 0; j < 9; j++) {board += "━━━┻";}
+							  board += "━━━┛\n";
+							  }
+			else {board += "┃";
+				  for (int j = 0; j < 9; j++) {board += "   ┃";}
+				  board += "   ┃\n";
+				  board += "┣";
+				  for (int j = 0; j < 9; j++) {board += "━━━╋";}
+				  board += "━━━┫\n";	
+				  }
+		}
 		
+		System.out.print(board);
     	}
     
 }
+
