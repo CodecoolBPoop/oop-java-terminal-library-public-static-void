0791bc13dd8968d8a1ee089c9d53708c95fe20ca
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-05-29 13:59:20 +0200

    Initial java file

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import com.codecool.termlib.Terminal;
+import com.codecool.termlib.Direction;
+import com.codecool.termlib.Attribute;
+import com.codecool.termlib.Color;
+
+import java.util.Scanner;
+import java.io.InputStream;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    
+}
