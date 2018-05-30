9f67c3111f78e0a12c09466f818f2f03896b6694
SinyiMark <35994165+SinyiMark@users.noreply.github.com>
2018-05-30 13:00:49 +0200

    wincondition check

--- src/PlayingField.java
+++ src/PlayingField.java
@@ -20,8 +20,55 @@ public class PlayingField {
     public void setFieldsValue(int x, int y, char value) {
         playingField[x][y] = value;
     }
-
-    
-    
-
+	public void win(player){
+		Terminal.clearScreen()
+		Terminal.command(player + "player win")
+	}
+	
+	private void rowAndColumnCheck(int modifyCordinate, int otherCordinate, String player){
+		for (int i = 4; i < - 4; i--) {
+			if(modifyCordinate, + i > -1 && modifyCordinate, + i < 10){
+				if(playingField[modifyCordinate, + i][otherCordinate] == player){
+					markCheck ++;
+					if( markCheck > 4){
+						win(player)
+					}
+				}else{
+					markCheck = 0
+					}				
+			}
+		}
+	}
+	
+    public winconditionCheck(int x, int y, int turnNumber, String player) {
+		int markCheck = 0;
+		if (turnNumber > 4){
+			rowAndColumnCheck(x,y,player)
+			rowAndColumnCheck(y,x,player)
+			for (int i = 4; i < - 4; i--) {
+				if((x + i > -1 && x + i < 10) && (y + i > -1 && y + i < 10)){
+					if(playingField[x + i][y + i] == player){
+						markCheck ++;
+						if( markCheck > 4){
+							win(player)
+					}else{
+						markCheck = 0
+						}				
+					}
+				}
+			}
+			for (int i = 4; i < - 4; i--) {
+				if((x + i > -1 && x + i < 10) && (y + i > -1 && y + i < 10)){
+					if(playingField[x + i][y - i] == player){
+						markCheck ++;
+						if( markCheck > 4){
+							win(player)
+					}else{
+						markCheck = 0
+						}				
+					}
+				}
+			}
+		}
+	}
 }
